0e34133b5e36769d5d3d08b680f047275aa2f9ba
Adam <iloscnd@gmail.com>
2018-11-24 17:10:04 +0100

    task2/4

---
+++ list4/task2.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import k_means, Birch
+
+n = 1000
+mi = np.array([[0,0],[4,0]])
+sigma = np.diag([1,100])
+points = np.random.normal(loc=mi.repeat(n/2,0), size=(n,2))
+points = sigma.dot(points.T).T
+
+points_normalized = (points - points.min(axis=0)) / (points.max(axis=0) - points.min(axis=0))
+points_standarized = (points - points.mean(axis=0)) / points.std(axis=0)
+
+
+_, label, _ = k_means(points,2)
+_, label_normalized, _ = k_means(points_normalized,2)
+_, label_standarized, _ = k_means(points_standarized,2)
+
+
+fig, axes = plt.subplots(2,3, figsize=(20,20))
+
+axes[0,0].scatter(points[:,0], points[:,1], c=label)
+axes[0,1].scatter(points[:,0], points[:,1], c=label_normalized)
+axes[0,2].scatter(points[:,0], points[:,1], c=label_standarized)
+
+birch = Birch(threshold=0.01, branching_factor=30, n_clusters=2)
+
+
+birch.fit(points)
+axes[1,0].scatter(points[:,0], points[:,1], c=birch.labels_)
+birch.fit(points_normalized)
+axes[1,1].scatter(points[:,0], points[:,1], c=birch.labels_)
+birch.fit(points_standarized)
+axes[1,2].scatter(points[:,0], points[:,1], c=birch.labels_)
+
+plt.show()
+
+
+
+#DBScan 
+
+
+
+
+
+
+
